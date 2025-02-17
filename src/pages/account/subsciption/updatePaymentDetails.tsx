2aa5727c83accccd9e524f85cb66a30eae4ec675
aboufarcha <aboufarcha@gmail.com>
2025-02-17 18:48:56 +0100

    feat: update payment details

--- src/pages/account/subsciption/updatePaymentDetails.tsx
+++ src/pages/account/subsciption/updatePaymentDetails.tsx
@@ -3,6 +3,13 @@ import AccountFooter from "../../../components/footer/AccountFooter";
 import Header from "../headers/Header";
 import { GoChevronLeft } from "react-icons/go";
 import AddressForm from "../../../components/account/AddressForm";
+import {
+  FaCcMastercard,
+  FaCcPaypal,
+  FaCcVisa,
+  FaPaypal,
+} from "react-icons/fa6";
+import { SiAmericanexpress, SiMastercard } from "react-icons/si";
 
 const UpdatePaymentDetails = () => {
   return (
@@ -38,8 +45,42 @@ const UpdatePaymentDetails = () => {
                 </a>{" "}
                 nous avons besoin de ces informations.
               </p>
-              <div className="max-w-[750px]">
-                <AddressForm isPaymentAddress="true"></AddressForm>
+              <AddressForm isPaymentAddress="true"></AddressForm>
+              <div className="rounded-sm border-2 border-neutral-500 w-full mt-5 ">
+                <div className="flex py-5 px-3">
+                  <input type="radio"></input>
+                  <div className="space-y-1 px-5">
+                    <p className="text-neutral-50 font-semibold">Visa</p>
+                    <div className="flex items-center ">
+                      <FaCcVisa className="text-neutral-50 h-8 w-8" />
+                      <p className="ml-2">*** 8932 \ 06/27 </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="rounded-sm border-2 border-neutral-500 w-full ">
+                <div className="flex py-5 px-3">
+                  <input type="radio"></input>
+                  <div className="space-y-1 px-5">
+                    <p className="text-neutral-50 font-semibold">Visa</p>
+                    <div className="flex items-center ">
+                      <FaCcVisa className="text-neutral-50 h-8 w-8" />
+                      <FaCcMastercard className=" h-8 w-8 ml-2" />
+                      <SiAmericanexpress className=" h-8 w-8 ml-2" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="rounded-sm border-2 border-neutral-500 w-full ">
+                <div className="flex py-5 px-3">
+                  <input type="radio"></input>
+                  <div className="space-y-1 px-5">
+                    <p className="text-neutral-50 font-semibold">Paypal</p>
+                    <div className="flex items-center ">
+                      <FaCcPaypal className="text-neutral-50 h-8 w-8" />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
